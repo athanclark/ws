b0a6b0892f02ab078d2bf6999f67390a29fa7781
Athan Clark <athan.clark@gmail.com>
2016-06-29 22:03:33 -0600

    more websocket-like

--- src/App.hs
+++ src/App.hs
@@ -11,7 +11,9 @@ import Network.URI
 import Network.WebSockets
 import Wuss
 
-import Data.Text (Text, pack)
+import qualified Data.Text               as T
+import qualified Data.Text.Lazy.Encoding as LT
+import qualified Data.Text.Lazy.IO       as LT
 import Control.Monad.IO.Class
 import Control.Monad.Reader
 import Control.Monad
@@ -31,15 +33,17 @@ app = do
     -- https://hackage.haskell.org/package/wuss-1.0.4/docs/Wuss.html
     ws conn = do
       void . forkIO . forever $ do
-        message <- receiveData conn
-        print (message :: Text)
+        message <- receiveDataMessage conn
+        LT.putStrLn . LT.decodeUtf8 $ case message of
+                                        Text   x -> x
+                                        Binary x -> x
 
       let loop = do
             userInput <- getLine
             unless (null userInput) $ do
-              sendTextData conn (pack userInput)
+              sendTextData conn (T.pack userInput)
               loop
 
       loop
 
-      sendClose conn (pack "Bye!")
+      sendClose conn (T.pack "Bye!")
